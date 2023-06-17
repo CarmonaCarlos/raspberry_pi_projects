c2e58e7a5515458595b1d06d0375b09397826dbe
CarmonaCarlos <carlos.carmona18@outlook.com>
2023-06-16 19:28:01 -0600

    Test

--- raspberry_pi_projects/iot/ultrasonic_sesor/ultrasonic_sensor.py
+++ raspberry_pi_projects/iot/ultrasonic_sesor/ultrasonic_sensor.py
@@ -1,4 +1,21 @@
-#Initial code
-#Test
+import RPi.GPIO as GPIO 
+import time
+
+
+led = 18
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led, GPIO.OUT)
+
+while(True):    
+    GPIO.output(led, GPIO.HIGH)
+    print("Led encendido")
+    time.sleep(2)
+    GPIO.output(led, GPIO.LOW)
+    print("Led apagado")
+    time.sleep(2)
+
+GPIO.cleanup()
 
 
